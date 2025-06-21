364c8c30db4868a558d52156a6a8d1892998b672
luke yeo <87899265+lukeskywalker22@users.noreply.github.com>
2025-06-21 23:25:47 +0800

    Added glow to invite page elements

--- lib/views/Home Page/invites.dart
+++ lib/views/Home Page/invites.dart
@@ -11,8 +11,8 @@ class _OrgInvitesPageState extends State<OrgInvitesPage> {
   var sampleOrgInvites = [
     "NUS High School of Science and Mathematics",
     "Anglo-Chinese School (Independent)",
-    "Nullspace robotics",
-    "brightSparks tuition centre",
+    "Nullspace robotics centre",
+    "BrightSparks tuition centre",
   ];
 
   var sampleProjectInvites = [
@@ -32,64 +32,78 @@ class _OrgInvitesPageState extends State<OrgInvitesPage> {
             "Organization invites",
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 10),
+          SizedBox(height: 5),
           SizedBox(
             width: MediaQuery.of(context).size.width - 150,
-            height: MediaQuery.of(context).size.height / 2 - 105,
+            height: MediaQuery.of(context).size.height / 2 - 95,
             child: ListView.separated(
               itemBuilder: (context, index) {
                 return Column(
                   children: [
-                    Card(
-                      child: ListTile(
-                        title: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              sampleOrgInvites[index],
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            Row(
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Theme.of(
+                              context,
+                            ).colorScheme.primary.withAlpha(150),
+                            blurRadius: 2,
+                            spreadRadius: 1,
+                          ),
+                        ],
+                      ),
+                      child: Card(
+                        child: ListTile(
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                sampleOrgInvites[index],
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    "34 members",
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                  VerticalDivider(),
+                                  Text(
+                                    "5 projects",
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          subtitle: Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Row(
                               children: [
-                                Text(
-                                  "34 members",
-                                  style: TextStyle(fontSize: 16),
+                                TextButton.icon(
+                                  onPressed: () {},
+                                  label: Text(
+                                    "Accept",
+                                    style: TextStyle(color: Colors.green),
+                                  ),
+                                  icon: Icon(Icons.check, color: Colors.green),
                                 ),
-                                VerticalDivider(),
-                                Text(
-                                  "5 projects",
-                                  style: TextStyle(fontSize: 16),
+                                SizedBox(height: 5),
+                                TextButton.icon(
+                                  onPressed: () {
+                                    setState(() {
+                                      sampleOrgInvites.removeAt(index);
+                                    });
+                                  },
+                                  label: Text(
+                                    "Decline",
+                                    style: TextStyle(color: Colors.red),
+                                  ),
+                                  icon: Icon(Icons.cancel, color: Colors.red),
                                 ),
                               ],
                             ),
-                          ],
-                        ),
-                        subtitle: Padding(
-                          padding: const EdgeInsets.only(top: 10),
-                          child: Row(
-                            children: [
-                              TextButton.icon(
-                                onPressed: () {},
-                                label: Text(
-                                  "Accept",
-                                  style: TextStyle(color: Colors.green),
-                                ),
-                                icon: Icon(Icons.check, color: Colors.green),
-                              ),
-                              SizedBox(height: 5),
-                              TextButton.icon(
-                                onPressed: () {
-                                  setState(() {
-                                    sampleOrgInvites.removeAt(index);
-                                  });
-                                },
-                                label: Text(
-                                  "Decline",
-                                  style: TextStyle(color: Colors.red),
-                                ),
-                                icon: Icon(Icons.cancel, color: Colors.red),
-                              ),
-                            ],
                           ),
                         ),
                       ),
@@ -98,63 +112,77 @@ class _OrgInvitesPageState extends State<OrgInvitesPage> {
                 );
               },
               separatorBuilder: (context, index) {
-                return SizedBox(height: 10);
+                return SizedBox(height: 15);
               },
               itemCount: sampleOrgInvites.length,
               shrinkWrap: true,
             ),
           ),
-          SizedBox(height: 20),
+          SizedBox(height: 15),
           Text(
             "Project invites",
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 10),
+          SizedBox(height: 5),
           SizedBox(
             width: MediaQuery.of(context).size.width - 150,
-            height: MediaQuery.of(context).size.height / 2 - 105,
+            height: MediaQuery.of(context).size.height / 2 - 95,
             child: ListView.separated(
               itemBuilder: (context, index) {
                 return Column(
                   children: [
-                    Card(
-                      child: ListTile(
-                        title: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              sampleProjectInvites[index],
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            Text("5 members", style: TextStyle(fontSize: 16)),
-                          ],
-                        ),
-                        subtitle: Padding(
-                          padding: const EdgeInsets.only(top: 10),
-                          child: Row(
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Theme.of(
+                              context,
+                            ).colorScheme.primary.withAlpha(150),
+                            blurRadius: 2,
+                            spreadRadius: 1,
+                          ),
+                        ],
+                      ),
+                      child: Card(
+                        child: ListTile(
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              TextButton.icon(
-                                onPressed: () {},
-                                label: Text(
-                                  "Accept",
-                                  style: TextStyle(color: Colors.green),
-                                ),
-                                icon: Icon(Icons.check, color: Colors.green),
+                              Text(
+                                sampleProjectInvites[index],
+                                style: TextStyle(fontSize: 20),
                               ),
-                              SizedBox(height: 5),
-                              TextButton.icon(
-                                onPressed: () {
-                                  setState(() {
-                                    sampleOrgInvites.removeAt(index);
-                                  });
-                                },
-                                label: Text(
-                                  "Decline",
-                                  style: TextStyle(color: Colors.red),
-                                ),
-                                icon: Icon(Icons.cancel, color: Colors.red),
-                              ),
+                              Text("5 members", style: TextStyle(fontSize: 16)),
                             ],
+                          ),
+                          subtitle: Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Row(
+                              children: [
+                                TextButton.icon(
+                                  onPressed: () {},
+                                  label: Text(
+                                    "Accept",
+                                    style: TextStyle(color: Colors.green),
+                                  ),
+                                  icon: Icon(Icons.check, color: Colors.green),
+                                ),
+                                SizedBox(height: 5),
+                                TextButton.icon(
+                                  onPressed: () {
+                                    setState(() {
+                                      sampleOrgInvites.removeAt(index);
+                                    });
+                                  },
+                                  label: Text(
+                                    "Decline",
+                                    style: TextStyle(color: Colors.red),
+                                  ),
+                                  icon: Icon(Icons.cancel, color: Colors.red),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -163,7 +191,7 @@ class _OrgInvitesPageState extends State<OrgInvitesPage> {
                 );
               },
               separatorBuilder: (context, index) {
-                return SizedBox(height: 10);
+                return SizedBox(height: 15);
               },
               itemCount: sampleProjectInvites.length,
               shrinkWrap: true,
